f97a78a7b439e00eeae06fd58d226ddc2f2f38c7
yxy <510987679@qq.com>
2016-12-31 16:12:48 +0800

    游戏.html提交

---
+++ 乘法表.php
@@ -0,0 +1,23 @@
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<title>乘法表</title>
+	</head>
+	<body>
+	<script type="text/javascript">
+
+    	for ( var x = 1; x <= 9; x++) 
+    	{
+ 
+		        for ( var y = 1; y <= x; y++) 
+		        {
+		            document.write(y+ "*" + x+ "=" + (x * y));
+		        }
+        document.write("<br />");
+    	}
+
+	</script>
+
+	</body>
+</html>
